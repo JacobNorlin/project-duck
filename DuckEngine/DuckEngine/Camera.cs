f965402adca475e57ecf86ffc4a8fefe5ba2dc3d
Bjorn Holm <bjh@kth.se>
2011-11-10 23:49:57 +0100

    First engine code

---
+++ DuckEngine/DuckEngine/Camera.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DuckEngine
+{
+    class Camera : Entity
+    {
+        Camera(Engine _owner)
+        {
+            owner = _owner;
+        }
+    }
+}
---
+++ DuckEngine/DuckEngine/ILogic.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DuckEngine
+{
+    public interface ILogic
+    {
+        void Update(Microsoft.Xna.Framework.GameTime a);
+    }
+}
