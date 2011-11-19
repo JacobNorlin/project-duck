1c0d99624fa17d6c962431d05b02dd30d6014aa3
Bjorn Holm <bjh@kth.se>
2011-11-19 03:30:50 +0100

    Added first basic map editing functionality

--- DuckGame/DuckGame/Game.cs
+++ DuckGame/DuckGame/Game.cs
@@ -6,6 +6,9 @@ using Microsoft.Xna.Framework;
 using DuckGame.Players;
 using Microsoft.Xna.Framework.Input;
 using System;
+using DuckEngine.Managers;
+using Jitter.Dynamics;
+using Jitter.LinearMath;
 
 namespace DuckGame
 {
@@ -15,13 +18,16 @@ namespace DuckGame
         private GameState gameState = GameState.Playing;
         PlayerCamera playerCamera;
         DebugCamera debugCamera;
+        private  RigidBody moving;
+        private JVector along;
         private Engine Owner;
 
         public override void Initialize(Engine _owner)
         {
-            Owner = _owner;
+            this.Owner = _owner;
             playerCamera = new PlayerCamera(Owner);
             debugCamera = new DebugCamera(Owner, Vector3.Zero);
+            Owner.addAll(this);
             Owner.Camera = playerCamera;
             Owner.Window.AllowUserResizing = true;
         }
@@ -38,6 +44,7 @@ namespace DuckGame
             if (input.Keyboard_IsKeyDown(Keys.D8)) {
                 gameState = GameState.Playing;
                 Owner.Camera = playerCamera;
+                Owner.MouseEventManager.OnMouseOver = Owner.MouseEventManager.DefaultOnMouseOver;
             }
             if (input.Keyboard_IsKeyDown(Keys.D9)) {
                 if (gameState != GameState.Editing)
@@ -47,6 +54,36 @@ namespace DuckGame
                     //+Vector3.Up only so you notice you've changed camera mode
                     debugCamera.Target = playerCamera.Player.Position;
                     Owner.Camera = debugCamera;
+                    Owner.MouseEventManager.OnMouseOver = lol;
+                }
+            }
+            editorMouseHandling(gameTime, input);
+        }
+
+        public void lol(GameTime gameTime, InputManager input,
+            RigidBody hitBody, JVector hitNormal, float hitFraction)
+        {
+            if (input.Mouse_WasButtonPressed(InputManager.MouseButton.Left))
+            {
+                moving = hitBody;
+                moving.IsActive = false;
+                along = hitNormal;
+            }
+        }
+
+        private void editorMouseHandling(GameTime gameTime, InputManager input)
+        {
+            if (moving != null) {
+                moving.IsActive = false;
+                if (input.Mouse_IsButtonDown(InputManager.MouseButton.Left))
+                {
+                    Point movement = input.Mouse_Movement();
+                    moving.Position += along * movement.Y * 0.05f;
+                }
+                else
+                {
+                    moving.IsActive = true;
+                    moving = null;
                 }
             }
         }
